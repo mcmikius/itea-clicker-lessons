a1fb723972d7013d0e52c25f9cfb8cce00c6c3f2
Mykhailo <mcmikius@icloud.com>
2019-08-15 20:50:31 +0300

    fuck fail

--- src/app/play-page/play-page.component.ts
+++ src/app/play-page/play-page.component.ts
@@ -1,5 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Player} from '../player';
+import { Component, OnInit } from '@angular/core';
+
+// import 'rxjs/add/observable/interval';
+import {Observable} from '@reactivex/rxjs';
+import '@reactivex/rxjs/dist/package/add/observable/interval';
+
 
 @Component({
   selector: 'app-play-page',
@@ -7,16 +11,25 @@ import {Player} from '../player';
   styleUrls: ['./play-page.component.css']
 })
 export class PlayPageComponent implements OnInit {
-  @Input()
-  player: Player;
-
-  constructor() { }
+  points = 500;
+  increaser = 1;
 
   ngOnInit() {
-    setInterval(() => {
-      this.player.workers.forEach(w => {
-        this.player.money += w.addMoney();
-      });
-    }, 1000);
+    Observable.interval(200).subscribe(
+      (interval) => {
+        this.points += this.increaser * 0.1;
+      }
+    );
+  }
+
+  increase() {
+    this.points++;
+  }
+
+  buy(multiplicator) {
+    if (this.points > (this.increaser * 20)) {
+      this.points = this.points - (this.increaser * 20);
+      this.increaser = this.increaser * multiplicator;
+    }
   }
 }
